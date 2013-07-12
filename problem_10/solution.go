8d001e3d0a8042fe355fae6ff1f1334976a89b5c
Bjorn Tipling <bjorn@ambientchill.com>
2013-07-12 10:30:21 -0700

    Stub out problem 10.

---
+++ problem_10/solution.go
@@ -0,0 +1,18 @@
+package main
+/*
+
+	Problem:
+
+	The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+	Find the sum of all the primes below two million.
+
+*/
+
+const LIMIT int = 2000000
+
+func main() {
+	answer := "nfi"
+	println("The answer is: ", answer)
+}
+
